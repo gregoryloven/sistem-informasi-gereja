ccc1978905b60c6177a079d8dac8ae2f09b86fe6
gregoryloven <gregoryseminari@gmail.com>
2022-11-23 19:36:29 +0700

    Pengecekan if role untuk halaman view

--- core-sistem/app/Http/Controllers/LaporanController.php
+++ core-sistem/app/Http/Controllers/LaporanController.php
@@ -12,121 +12,163 @@ use App\Models\Perkawinan;
 use App\Models\Riwayat;
 use Carbon\Carbon;
 use DB;
-
+use Auth;
 
 class LaporanController extends Controller
 {
     public function baptis()
     {
-        $data = ListEvent::where('jenis_event', 'like', 'Baptis B%')
-        ->orderby('jadwal_pelaksanaan', 'ASC')
-        ->get();
-
-        $array = [];
-        foreach($data as $d)
+        if(Auth::user()->role != 'admin')
         {
-            $jumlah_baptis_bayi = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
-            ->where([['riwayats.status', 'Selesai'], ['list_events.id', $d->id]])->count();
-
-            array_push($array, $jumlah_baptis_bayi);
+            return back();
         }
-
-        return view('laporan.baptis',compact("data", "array"));
+        else
+        {
+            $data = ListEvent::where('jenis_event', 'like', 'Baptis B%')
+            ->orderby('jadwal_pelaksanaan', 'ASC')
+            ->get();
+    
+            $array = [];
+            foreach($data as $d)
+            {
+                $jumlah_baptis_bayi = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
+                ->where([['riwayats.status', 'Selesai'], ['list_events.id', $d->id]])->count();
+    
+                array_push($array, $jumlah_baptis_bayi);
+            }
+    
+            return view('laporan.baptis',compact("data", "array"));
+        }
     }
 
     public function baptisDewasa()
     {
-        $data = ListEvent::where('jenis_event', 'like', 'Baptis D%')
-        ->orderby('jadwal_pelaksanaan', 'ASC')
-        ->get();
-
-        $array = [];
-        foreach($data as $d)
+        if(Auth::user()->role != 'admin')
         {
-            $jumlah_baptis_dewasa = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
-            ->where([['riwayats.status', 'Selesai'], ['list_events.id', $d->id]])->count();
-
-            array_push($array, $jumlah_baptis_dewasa);
+            return back();
         }
-
-        return view('laporan.baptisDewasa',compact("data", "array"));
+        else
+        {
+            $data = ListEvent::where('jenis_event', 'like', 'Baptis D%')
+            ->orderby('jadwal_pelaksanaan', 'ASC')
+            ->get();
+    
+            $array = [];
+            foreach($data as $d)
+            {
+                $jumlah_baptis_dewasa = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
+                ->where([['riwayats.status', 'Selesai'], ['list_events.id', $d->id]])->count();
+    
+                array_push($array, $jumlah_baptis_dewasa);
+            }
+    
+            return view('laporan.baptisDewasa',compact("data", "array"));
+        }
     }
 
     public function komuni()
     {
-        $data = ListEvent::where('jenis_event', 'like', 'Ko%')
-        ->orderby('jadwal_pelaksanaan', 'ASC')
-        ->get();
-
-        $array = [];
-        $array2 = [];
-        foreach($data as $d)
+        if(Auth::user()->role != 'admin')
         {
-            $jumlah_komuni = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
-            ->where([['riwayats.status', 'Selesai'], ['list_events.id', $d->id]])->count();
-
-            $jumlah_lulus_kursus = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
-            ->where([['kursus', 'Lulus'], ['list_events.id', $d->id]])->count();
-
-            array_push($array, $jumlah_komuni);
-            array_push($array2, $jumlah_lulus_kursus);
+            return back();
         }
-
-        return view('laporan.komuni',compact("data", "array", "array2"));
+        else
+        {
+            $data = ListEvent::where('jenis_event', 'like', 'Ko%')
+            ->orderby('jadwal_pelaksanaan', 'ASC')
+            ->get();
+    
+            $array = [];
+            $array2 = [];
+            foreach($data as $d)
+            {
+                $jumlah_komuni = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
+                ->where([['riwayats.status', 'Selesai'], ['list_events.id', $d->id]])->count();
+    
+                $jumlah_lulus_kursus = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
+                ->where([['kursus', 'Lulus'], ['list_events.id', $d->id]])->count();
+    
+                array_push($array, $jumlah_komuni);
+                array_push($array2, $jumlah_lulus_kursus);
+            }
+    
+            return view('laporan.komuni',compact("data", "array", "array2"));
+        }
     }
 
     public function krisma()
     {
-        $data = ListEvent::where('jenis_event', 'like', 'Kr%')
-        ->orderby('jadwal_pelaksanaan', 'ASC')
-        ->get();
-
-        $array = [];
-        $array2 = [];
-        foreach($data as $d)
+        if(Auth::user()->role != 'admin')
         {
-            $jumlah_krisma = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
-            ->where([['riwayats.status', 'Selesai'], ['list_events.id', $d->id]])->count();
-
-            $jumlah_lulus_kursus = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
-            ->where([['kursus', 'Lulus'], ['list_events.id', $d->id]])->count();
-
-            array_push($array, $jumlah_krisma);
-            array_push($array2, $jumlah_lulus_kursus);
+            return back();
         }
-
-        return view('laporan.krisma',compact("data", "array", "array2"));
+        else
+        {
+            $data = ListEvent::where('jenis_event', 'like', 'Kr%')
+            ->orderby('jadwal_pelaksanaan', 'ASC')
+            ->get();
+    
+            $array = [];
+            $array2 = [];
+            foreach($data as $d)
+            {
+                $jumlah_krisma = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
+                ->where([['riwayats.status', 'Selesai'], ['list_events.id', $d->id]])->count();
+    
+                $jumlah_lulus_kursus = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
+                ->where([['kursus', 'Lulus'], ['list_events.id', $d->id]])->count();
+    
+                array_push($array, $jumlah_krisma);
+                array_push($array2, $jumlah_lulus_kursus);
+            }
+    
+            return view('laporan.krisma',compact("data", "array", "array2"));
+        }
     }
 
     public function kpp()
     {
-        $data = ListEvent::where('jenis_event', 'like', 'Kursus%')->get();
-
-        $array = [];
-        $array2 = [];
-        foreach($data as $d)
+        if(Auth::user()->role != 'admin')
         {
-            $jumlah_lulus_kursus = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
-            ->where([['kursus', 'Lulus'], ['list_events.id', $d->id]])->count();
-
-            $jumlah_tidak_lulus_kursus = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
-            ->where([['kursus', 'Tidak Lulus'], ['list_events.id', $d->id]])->count();
-
-            array_push($array, $jumlah_lulus_kursus);
-            array_push($array2, $jumlah_tidak_lulus_kursus);
+            return back();
         }
+        else
+        {
+            $data = ListEvent::where('jenis_event', 'like', 'Kursus%')->get();
 
-        return view('laporan.kpp',compact("data", "array", "array2"));
+            $array = [];
+            $array2 = [];
+            foreach($data as $d)
+            {
+                $jumlah_lulus_kursus = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
+                ->where([['kursus', 'Lulus'], ['list_events.id', $d->id]])->count();
+    
+                $jumlah_tidak_lulus_kursus = Riwayat::join('list_events', 'riwayats.list_event_id', '=', 'list_events.id')
+                ->where([['kursus', 'Tidak Lulus'], ['list_events.id', $d->id]])->count();
+    
+                array_push($array, $jumlah_lulus_kursus);
+                array_push($array2, $jumlah_tidak_lulus_kursus);
+            }
+    
+            return view('laporan.kpp',compact("data", "array", "array2"));
+        }
     }
 
     public function perkawinan(Request $request)
     {        
-        $data = Perkawinan::orderby('tanggal_perkawinan', 'DESC')->get();
-        $tahun = $request->datetimepicker3;
-
-        $data2 = Perkawinan::whereYear('tanggal_perkawinan', $request->datetimepicker3)->get();
-
-        return view('laporan.perkawinan',compact('data','data2'));
+        if(Auth::user()->role != 'admin')
+        {
+            return back();
+        }
+        else
+        {
+            $data = Perkawinan::orderby('tanggal_perkawinan', 'DESC')->get();
+            $tahun = $request->datetimepicker3;
+    
+            $data2 = Perkawinan::whereYear('tanggal_perkawinan', $request->datetimepicker3)->get();
+    
+            return view('laporan.perkawinan',compact('data','data2'));
+        }
     }
 
 }
