da0547147e639d558d65c5220baa388947597698
gregoryloven <gregoryseminari@gmail.com>
2022-11-25 00:14:19 +0700

    Update model tenant

--- core-sistem/app/Models/Tenant.php
+++ core-sistem/app/Models/Tenant.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tenant extends Model
 {
     use HasFactory;
+    protected $connection = 'landlord';
     protected $table = 'tenants';
     protected $primaryKey = 'id';
     public $timestamps = true;
